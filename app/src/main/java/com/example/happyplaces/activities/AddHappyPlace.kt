1c1216c66a3c4a78a5d7b431af1ac67f10e83bb8
amr mustafa <amrmustafa6051@gmail.com>
2022-10-31 00:51:09 +0200

    update open gallery and camara by using API 29

--- app/src/main/java/com/example/happyplaces/activities/AddHappyPlace.kt
+++ app/src/main/java/com/example/happyplaces/activities/AddHappyPlace.kt
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -32,16 +33,75 @@ import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
-@Suppress("DEPRECATION")
+
+
 class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
 
     companion object {
-        private const val GALLERY_REQUEST_CODE = 100
-        private const val CAMARA_REQUEST_CODE = 200
         private const val IMAGE_DIR = "HappyPlacesImages"
     }
 
+    /*
+     prepare result launch for each activity
+    */
+    private var resultLauncherForCamara =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (result.resultCode == Activity.RESULT_OK) {
+                try {
+                    val photoUri =
+                        savePhotoOnInternalStorage((data!!.extras!!.get("data") as Bitmap))
+                    binding.addImage.setImageURI(photoUri)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    // print error message
+                    Toast.makeText(
+                        this@AddHappyPlace,
+                        "Can not capture image from camara",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+            }
+        }
+
+    private var resultLauncherForGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                if (data != null) {
+                    // get uri about photo
+                    val photoUri = data.data
+
+                    try {
+
+                        // convert photo to bitmap
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, photoUri)
+
+                        // save and set photo
+                        val photoUri = savePhotoOnInternalStorage(selectedImageBitmap)
+                        binding.addImage.setImageURI(photoUri)
+                        binding.addImage.setImageBitmap(selectedImageBitmap)
+
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        // print error message
+                        Toast.makeText(
+                            this@AddHappyPlace,
+                            "Can not load image from gallery",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
+
     private lateinit var binding: ActivityAddHappyPlaceBinding
+
 
     // make object from calender
     private var calendar = Calendar.getInstance()
@@ -91,7 +151,7 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
 
     private fun showDatePicker() {
         // set date on today
-        dateListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+        dateListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
             calendar.set(Calendar.YEAR, year)
             calendar.set(Calendar.MONTH, month)
             calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
@@ -204,13 +264,14 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
 
     private fun openCaptureCamara() {
         val camaraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(camaraIntent, CAMARA_REQUEST_CODE)
+        resultLauncherForCamara.launch(camaraIntent)
     }
 
     private fun openGallery() {
         val galleryIntent = Intent(Intent.ACTION_PICK)
         galleryIntent.type = "image/*"
-        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+
+        resultLauncherForGallery.launch(galleryIntent)
     }
 
     private fun showSettingsDialogAndGoIt() {
@@ -263,62 +324,7 @@ class AddHappyPlace : AppCompatActivity(), View.OnClickListener {
 
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-
-        /*
-         here handle pick picture from gallery
-         */
-        if (resultCode == Activity.RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
-            if (data != null) {
-                // get uri about photo
-                val photoUri = data.data
-
-                try {
-
-                    // convert photo to bitmap
-                    val selectedImageBitmap =
-                        MediaStore.Images.Media.getBitmap(this.contentResolver, photoUri)
-
-                    // save and set photo
-                    val uri = savePhotoOnInternalStorage(selectedImageBitmap)
-                    binding.addImage.setImageBitmap(selectedImageBitmap)
-
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    // print error message
-                    Toast.makeText(
-                        this@AddHappyPlace,
-                        "Can not load image from gallery",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
-        }
-
-        /*
-        here handle capture photo from camara
-         */
-        if (resultCode == Activity.RESULT_OK && requestCode == CAMARA_REQUEST_CODE && data != null) {
-            try {
-                val photoUri = savePhotoOnInternalStorage((data.extras!!.get("data") as Bitmap))
-                binding.addImage.setImageURI(photoUri)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                // print error message
-                Toast.makeText(
-                    this@AddHappyPlace,
-                    "Can not capture image from camara",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            }
-
-        }
-    }
 
 
 }
